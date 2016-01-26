4e60dba193f284d33a58e64d19689dccee63ca39
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2016-01-26 02:39:06 -0500

    Let's make it sassier

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import emoji
 import requests
 import json
 import re
+import logging
+import random
 
+from datetime import datetime
+from riotwatcher import RiotWatcher
 from secrets import *
+
+logging.basicConfig(level=logging.INFO)
 
 client = discord.Client()
 client.login(USERNAME, PASSWORD)
+
+riot = RiotWatcher(RIOT_KEY)
 
 def get_champ_info(name):
     res = requests.get("http://api.champion.gg/champion/" + name.lower(), params={'api_key': CHAMPION_GG_KEY}).json()
@@ -26,6 +34,14 @@ def get_champ_info(name):
     return response
 
 
+def get_last_match(summoner):
+    s = riot.get_summoner(name=summoner)
+    matches = riot.get_match_list(s['id'])
+    if not matches:
+        return None
+    return matches['matches'][0]
+
+
 @client.event
 def on_message(message):
     if message.content.startswith('!hello') and '<@140959950816935937>' in message.content:
@@ -39,12 +55,73 @@ def on_message(message):
         else:
             client.send_message(message.channel, get_champ_info(champion[0]))
 
+
+@client.event
+def on_member_update(before, after):
+    summoner = "JCena4Pres"
+    if random.random() > 0.50:
+        summoner = "jc4p"
+    last_match = get_last_match(summoner)
+    if not last_match:
+        pass
+    last_match_time = datetime.fromtimestamp(last_match['timestamp'] / 1000)
+    delta = (datetime.utcnow() - last_match_time).total_seconds()
+    if delta > 1800:
+        pass
+    match = riot.get_match(last_match['matchId'])
+    player = None
+    for p in match['participantIdentities']:
+        if p['player']['summonerName'] == summoner:
+            player = p
+            break
+    details = None
+    for d in match['participants']:
+        if p['participantId'] == player['participantId']:
+            details = d
+            break
+    team = match['teams'][0] if match['teams']['0']['teamId'] == player['teamId'] else match['teams'][1]
+
+    won = team['winner']
+    champ = riot.static_get_champion(details['championId'])
+    kills = details['kills']
+    deaths = details['deaths']
+    assists = details['assists']
+    kda = (kills + assists) / (deaths * 1.0)
+    did_most_damage = all([x['stats']['totalDamageDealtToChamps'] <= details['stats']['totalDamageDealtToChamps'] for x in match['participants']])
+
+    discord_user = None
+    for m in client.servers[0].members:
+        if summoner == "JCena4Pres" and m['name'] == 'imsteaky':
+            discord_user = m
+            break
+        if summoner == 'jc4p' and m['name'] == 'kasra':
+            discord_user = m
+            break
+
+    message = "Hey <@{}> ".format(discord_user.id)
+    if won:
+        if did_most_damage:
+            message += "nice {}-{}-{} {} moves, you did the most damage too!".format(kills, deaths, assists, champ['name'])
+        elif kda > 1.0:
+            message += "gg {}-{}-{} sick {} {} kda bro".format(kills, deaths, assists, champ['name'], champ['kda'])
+    else:
+        if kda > 1.0:
+            message += "gg. {}-{}-{}, you still lose. guess you can't carry as {}".format(kills, deaths, assists, champ['name'])
+        else if not did_most_damage:
+            message += "Guess what? YOUR :clap: {} :clap: {} : clap {} :clap: {} :clap: DOES :clap: NO :clap: DAMAGE :clap:".format(kills, deaths, assists, champ['name'])
+       else:
+            message += "lol nice {}-{}-{} {} loss".format(kills, deathsm assists, champ['name'])
+    client.send_message(client.servers[0].channels[0], message)
+
+
 @client.event
 def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print 'Logged in as'
+    print client.user.name
+    print client.user.id
+    print client.servers[0].__dict__
+
+    print '------'
 
 if __name__ == "__main__":
     try:
--- secrets.py
+++ secrets.py
@@ -4,3 +4,4 @@ USERNAME = os.environ.get('DISCORD_EMAIL',    '')
 PASSWORD = os.environ.get('DISCORD_PASSWORD', '')
 
 CHAMPION_GG_KEY = os.environ.get('CHAMPION_GG_KEY', '')
+RIOT_KEY = os.environ.get('RIOT_KEY', '')
