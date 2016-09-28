f5e821d917bad36dc444cdd1e2dd8ee6c41fb3cc
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2016-09-28 00:53:00 -0600

    New clan

--- main.py
+++ main.py
@@ -36,6 +36,21 @@ def get_champ_info(name):
     return response
 
 
+def get_player_info(summoner):
+    s = riot.get_summoner(name=summoner)
+    if not s:
+        return "No results"
+    league = riot.get_league_entry(summoner_ids=[s['id']])
+    if not league or str(s['id']) not in league:
+        return "No results"
+    stats = league[str(s['id'])][0]
+    message = "{} is {} {}".format(s['name'], stats['tier'].lower(), stats['entries'][0]['division'])
+    last_matches = get_last_matches(s['name'])
+    last_match_message = get_latest_match_response(s['name'].lower(), last_matches)
+    if last_match_message:
+        message += "\n" + last_match_message
+    return message
+
 def get_last_matches(summoner, classic_only=False):
     s = riot.get_summoner(name=summoner)
     matches = riot.get_recent_games(s['id'])
@@ -44,6 +59,51 @@ def get_last_matches(summoner, classic_only=False):
     if not classic_only:
         return matches['games']
     return [x for x in matches['games'] if x['gameMode'] == 'CLASSIC']
+
+def get_latest_match_response(summoner, last_matches):
+    match = last_matches[0]
+    last_match_start = datetime.utcfromtimestamp(match['createDate'] / 1000)
+    delta = (datetime.utcnow() - last_match_start).total_seconds()
+    if delta > 3600:
+        print "Delta too high: {}".format(delta)
+        return
+
+    stats = match['stats']
+    won = stats['win']
+    champ = riot.static_get_champion(match['championId'])
+    kills = 0 if 'championsKilled' not in stats else stats['championsKilled']
+    deaths = 0 if 'numDeaths' not in stats else stats['numDeaths']
+    assists = 0 if 'assists' not in stats else stats['assists']
+    kda = (kills + assists) / (deaths * 1.0)
+
+    discord_user = None
+    #TODO: idk why I wrote this code like this, the old one was like this.
+    for m in client.servers[1].members:
+        if summoner == "bearwhale" and m.name == 'bearwhale':
+            discord_user = m
+            break
+        if summoner == 'jc4p' and m.name == 'kasra':
+            discord_user = m
+            break
+        if summoner == 'burlychan' and m.name == 'deadbeat':
+            discord_user = m
+            break
+
+    message = "Hey <@{}> ".format(discord_user.id)
+    if won:
+        if kda > 1.0:
+            message += "gg. {}-{}-{} sick {} {} kda bro".format(kills, deaths, assists, champ['name'], kda)
+        else:
+            return
+    else:
+        if kda > 1.0:
+            message += "gg. {}-{}-{}, you still lose. guess you can't carry as {}".format(kills, deaths, assists, champ['name'])
+        # elif random.random() > 0.10:
+        #     message += "Guess what? YOUR :clap: {} :clap: {} : clap {} :clap: {} :clap: DOES :clap: NO :clap: DAMAGE :clap:".format(kills, deaths, assists, champ['name'])
+        else:
+            message += "Guess what? YOUR :clap: {} :clap: {} :clap: {} :clap: {} :clap: DOES :clap: NOTHING :clap:".format(kills, deaths, assists, champ['name'])
+    return message
+
 
 
 @client.event
@@ -58,6 +118,14 @@ def on_message(message):
             client.send_message(message.channel, "I don't know who that is, try !champgg {Champion Name}")
         else:
             client.send_message(message.channel, get_champ_info(champion[0]))
+    elif message.content.startswith('!player'):
+        player = re.findall(r'\!player[\t ]+(\w+)?\D?', message.content)
+        print player
+        if not player or len(player) > 1:
+            client.send_message(message.channel, "I don't know who that is, try !player {Summoner Name}")
+        else:
+            client.send_message(message.channel, get_player_info(player[0]))
+
 
 
 @client.event
@@ -66,51 +134,16 @@ def on_member_update(before, after):
     if (datetime.utcnow() - last_sent).total_seconds() < 1800:
         return
 
-    summoner = "JCena4Pres"
-    if random.random() > 0.50:
-        summoner = "jc4p"
+    # TODO: Pick a summoner?
+    return
     last_matches = get_last_matches(summoner, classic_only=True)
     if not last_matches:
         return
 
-    match = last_matches[0]
-    last_match_start = datetime.fromtimestamp(match['createDate'] / 1000)
-    delta = (datetime.utcnow() - last_match_start).total_seconds()
-    if delta > 3600:
-        return
-
-    stats = match['stats']
-    won = stats['win']
-    champ = riot.static_get_champion(match['championId'])
-    kills = stats['championsKilled']
-    deaths = stats['numDeaths']
-    assists = stats['assists']
-    kda = (kills + assists) / (deaths * 1.0)
-
-    discord_user = None
-    for m in client.servers[0].members:
-        if summoner == "JCena4Pres" and m.name == 'imsteaky':
-            discord_user = m
-            break
-        if summoner == 'jc4p' and m.name == 'kasra':
-            discord_user = m
-            break
-
-    message = "Hey <@{}> ".format(discord_user.id)
-    if won:
-        if kda > 1.0:
-            message += "gg. {}-{}-{} sick {} {} kda bro".format(kills, deaths, assists, champ['name'], kda)
-        else:
-            return
-    else:
-        if kda > 1.0:
-            message += "gg. {}-{}-{}, you still lose. guess you can't carry as {}".format(kills, deaths, assists, champ['name'])
-        elif random.random() > 0.50:
-            message += "Guess what? YOUR :clap: {} :clap: {} : clap {} :clap: {} :clap: DOES :clap: NO :clap: DAMAGE :clap:".format(kills, deaths, assists, champ['name'])
-        else:
-            return
-    client.send_message(client.servers[0].channels[0], message)
-    last_sent = datetime.utcnow()
+    message = get_latest_match_response(last_matches)
+    if message:
+        client.send_message(client.servers[0].channels[0], message)
+        last_sent = datetime.utcnow()
 
 
 @client.event
