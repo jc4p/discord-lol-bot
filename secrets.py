14db9d5b07b310279daee62200711626805663e1
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2016-09-28 00:59:29 -0600

    Fix up .gitignore

--- secrets.py
+++
@@ -1,7 +0,0 @@
-import os
-
-USERNAME = os.environ.get('DISCORD_EMAIL',    '')
-PASSWORD = os.environ.get('DISCORD_PASSWORD', '')
-
-CHAMPION_GG_KEY = os.environ.get('CHAMPION_GG_KEY', '')
-RIOT_KEY = os.environ.get('RIOT_KEY', '')
